3c654a77f56e98b148e9a7109e1055ed94de445d
mzanella <zanna0150@gmail.com>
2018-11-01 11:43:04 +0100

    Fix UDP proxy, now it is able to create the header with right src and dst to us with the chain

--- udpproxy/udpproxy.py
+++ udpproxy/udpproxy.py
@@ -19,15 +19,25 @@ logger.addHandler(handler)
 def parse_args():
     parser = OptionParser()
 
-    parser.add_option('--bind-address',
+    parser.add_option('--proxy-ip',
                       help='The address to bind, 0.0.0.0 for all ip address.')
-    parser.add_option('--port',
+    parser.add_option('--proxy-port',
                       help='The port to listen, eg. 623.',
                       type=int)
+    parser.add_option('--src-ip',
+                      help='Source ip, eg. 192.168.3.101.')
+    parser.add_option('--src-port',
+                      help='Source port, eg. 623.',
+                      type=int)
     parser.add_option('--dst-ip',
-                      help='Destination host ip, eg. 192.168.3.101.')
+                      help='Destination ip, eg. 192.168.3.101.')
     parser.add_option('--dst-port',
-                      help='Destination host port, eg. 623.',
+                      help='Destination port, eg. 623.',
+                      type=int)
+    parser.add_option('--ingress-ip',
+                      help='Ingress ip, eg. 192.168.3.101.')
+    parser.add_option('--ingress-port',
+                      help='Ingress port, eg. 623.',
                       type=int)
 
     return parser.parse_args()
@@ -38,8 +48,8 @@ def parse_args():
 def recv():
     sock_src = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     sock_dst = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    recv_addr = (options.bind_address, options.port)
-    dst_addr = (options.dst_ip, options.dst_port)
+    recv_addr = (options.proxy_ip, options.proxy_port)
+    dst_addr = (options.ingress_ip, options.ingress_port)
     sock_src.bind(recv_addr)
 
     while True:
@@ -48,8 +58,9 @@ def recv():
             logger.error('an error occured')
             break
         logger.debug('received: {0!r} from {1}'.format(data, addr))
-        pkt = IP(src=options.bind_address, dst=options.dst_ip) / \
-            UDP(sport=options.port, dport=options.dst_port)
+        pkt = IP(src=options.src_ip, dst=options.dst_ip) / \
+            UDP(sport=options.src_port, dport=options.dst_port)
+        pkt.show()
         data = raw(pkt) + data
         sock_dst.sendto(data, dst_addr)
         data, _ = sock_dst.recvfrom(65565)
@@ -62,6 +73,9 @@ def recv():
 if __name__ == '__main__':
     parse_args()
     try:
+        #pkt = IP(src=options.src_ip, dst=options.dst_ip) / \
+        #    UDP(sport=options.src_port, dport=options.dst_port)
+        #pkt.show()
         recv()
     except KeyboardInterrupt:
         exit(0)
